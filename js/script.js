66a2e53026effdbcfd2f0c55be1d33229c6c1366
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-11-09 16:17:54 +0100

    Adjustment on navbar
    Still missing functions to close search and menu

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const hamburgerToggle = document.querySelector(".fa-bars");
+const hamburgerMenu = document.querySelector(".nav-menu");
+
+hamburgerToggle.addEventListener('click', function(){
+    hamburgerMenu.classList.toggle('active');
+    console.log(event); 
+});
+
+
+
+const searchToggle = document.querySelector(".fa-magnifying-glass");
+const searchField = document.querySelector(".search-field");
+
+searchToggle.addEventListener('click', function(){
+    searchField.classList.toggle('search');
+})
