5c880488d891890102c822b568b066602de7c8e6
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-11-17 17:32:09 +0100

    Trying to access content in WP posts

--- js/index.js
+++ js/index.js
@@ -1,20 +1,51 @@
 const contentContainer = document.querySelector(".content");
 
-const url = "https://fredo.one/nocreaseblog/wp-json/wc/store/products/";
+// const url = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts";
+
+// async function getContent() {
+//     try {
+//         const response = await fetch(url);
+//         const results = await response.json()
+
+//         // console.log(results[0])
+
+//         for (let i = 0; i < results.length; i++) {
+//             console.log(results[i])
+
+//             contentContainer.innerHTML += `<div>${results[0].content.rendered}"</div>`;
+
+//         }
+
+
+//     }
+
+//     catch(error) {
+//         console.log(error);
+//     }
+// };
+
+getContent()    
+
+
+// contentContainer.innerHTML += `<div><img class="image" src="${results[0].images[0].src}"</div>
+//                                         <div>${results[0].description}</div>`;
+
+
+const url = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts/?id=59";
 
 async function getContent() {
     try {
         const response = await fetch(url);
         const results = await response.json()
 
-        console.log(results[0].content)
+        // console.log(results[0])
 
-        for (let i = 0; i < results.length; i++) {
-            console.log(results[i])
-        }
 
-        contentContainer.innerHTML += `<div><img class="image" src="${results[0].images[0].src}"</div>
-                                        <div>${results[0].description}</div>`;
+        contentContainer.innerHTML += `<div class="details_container">
+                                        <h1 class="details_title">${results[0].title.rendered}</h1>
+                                        <div class="details_content">${results[0].content.rendered}"</div>
+                                        </div>`;
+
 
     }
 
@@ -23,4 +54,4 @@ async function getContent() {
     }
 };
 
-getContent()
+getContent() 
