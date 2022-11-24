ee2783f5f64e9c66a351f6d668e988dd9aa96325
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-11-24 23:25:28 +0100

    small css adjustment on nav

--- js/posts.js
+++ js/posts.js
@@ -1,38 +1,49 @@
-// let loadingButton = document.querySelector(".loading-button");
-// let currentPosts = 4;
 const postsContainer = document.querySelector(".posts_container");
 const showMoreBtn = document.querySelector(".show-more")
 
 const url = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts?per_page=100&_embed";
 
 async function getContent() {
+    
     try {
         const response = await fetch(url);
         const results = await response.json()
 
-        console.log(results[0])
-        let postIndex = 2;
-        showMoreBtn.onclick = function() {
-            
+        const limit = 3
+        let startindex = 0
+        let endindex = limit
         
-        
-        
-        for (let i = 0; i < results.length; i++) {
-            // console.log(results[i])
-            postsContainer.innerHTML += `<div class="post_container">
-                                            <div class"post-image_container">
-                                                <img src="${results[i]._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" />
-                                            </div>
-                                            <div class="post-info">
-                                                <a href="/article.html?id=${results[i].id}">
-                                                <h2>${results[i].title.rendered}</h2>
-                                                </a>
-                                                <div class="category-tag carousel-tag">${results[i]._embedded["wp:term"][0][0].name}</div>
-                                                <p>${results[i].excerpt.rendered}</p>
-                                            </div>
-                                        </div>`;
-
+        const addArticle = (article) => {
+            return`<div class="post_container">
+                    <div class"post-image_container">
+                        <img src="${article._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" />
+                    </div>
+                    <div class="post-info">
+                        <a href="/article.html?id=${article.id}">
+                        <h2>${article.title.rendered}</h2>
+                        </a>
+                        <div class="category-tag carousel-tag">${article._embedded["wp:term"][0][0].name}</div>
+                        <p>${article.excerpt.rendered}</p>
+                    </div>
+                </div>`;
         }
+        const articlesToAdd = results.slice(startindex, endindex)
+        articlesToAdd.forEach((article) => {
+            postsContainer.innerHTML += addArticle(article);
+
+        })                            
+        showMoreBtn.onclick = function addMoreArticles() {
+            startindex = startindex + limit
+            endindex = endindex + limit
+            const articlesToAdd = results.slice(startindex, endindex)
+            // add setTimeoute and add spinner here
+            articlesToAdd.forEach((article) => {
+                postsContainer.innerHTML += addArticle(article);
+            })      
+
+            if (endindex >= results.length){
+                showMoreBtn.remove()
+            }                     
         }
     }
 
@@ -42,29 +53,3 @@ async function getContent() {
 };
 
 getContent()  
-// showMoreBtn.onclick = function(btnValue) {
-//     // console.log(event.target.value)
-//     let btnValue = event.target.value + 2;
-//     return btnValue;
-// }
-
-// console.log(btnValue)
-
-// {/* <script>
-// const loadmore = document.querySelector('#loadmore');
-//     let currentItems = 2;
-//     loadmore.addEventListener('click', (e) => {
-//         const elementList = [...document.querySelectorAll('.list .list-element')];
-//         for (let i = currentItems; i < currentItems + 2; i++) {
-//             if (elementList[i]) {
-//                 elementList[i].style.display = 'block';
-//             }
-//         }
-//         currentItems += 2;
-
-//         // Load more button will be hidden after list fully loaded
-//         if (currentItems >= elementList.length) {
-//             event.target.style.display = 'none';
-//         }
-//     })
-// </script> */}
