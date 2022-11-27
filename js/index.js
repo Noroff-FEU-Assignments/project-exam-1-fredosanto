15ee6e27007d38907db474f495bab74789ddc6e3
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-11-27 19:20:19 +0100

    carousel adjustment

--- js/index.js
+++ js/index.js
@@ -10,22 +10,18 @@ async function getContent() {
 
         // console.log(results[0])
 
-        for (let i = 0; i < results.length; i++) {
-            // console.log(results[i])
-
+        results.forEach((article) => {
             contentContainer.innerHTML += `<div class="carousel-post">
-                                                <img src="${results[i]._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" />
+                                                <img src="${article._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" />
                                                 <div class="carousel-post-info">
-                                                    <a href="/article.html?id=${results[i].id}">
-                                                    <h3>${results[i].title.rendered}</h3>
+                                                    <a href="/article.html?id=${article.id}">
+                                                    <h3>${article.title.rendered}</h3>
                                                     </a>
-                                                    <div class="category-tag carousel-tag">${results[i]._embedded["wp:term"][0][0].name}</div>
-                                                    <p>${results[i].excerpt.rendered}</p>
+                                                    <div class="category-tag carousel-tag">${article._embedded["wp:term"][0][0].name}</div>
+                                                    <p>${article.excerpt.rendered}</p>
                                                 </div>
                                             </div>`;
-
-        }
-
+        })
 
     }
 
@@ -36,38 +32,72 @@ async function getContent() {
 
 getContent()    
 
-document.addEventListener("click", carouselEvent => {
-    let carouselClick
-    if (carouselEvent.target.matches(".slide-arrow")) {
-        carouselClick = carouselEvent.target
-    } else {
-        carouselClick = carouselEvent.target.closest(".slide-arrow")
+function init() {
+    const arrowLeft = document.querySelector(".left-arrow")
+    const arrowRight = document.querySelector(".right-arrow")
+    const contentContainer = document.querySelector(".carousel");
+    console.log(arrowRight)
+    function handleNavigation(interaction) {
+        const {clientWidth, scrollLeft} = contentContainer;
+        console.log(clientWidth)
+        console.log('contentContainer', contentContainer)
+        // const carouselWidth = contentContainer.clientWidth;
+        // const scrollLeft = contentContainer.scrollLeft;
+        let scrollX 
+            if(interaction === 'left') {
+                scrollX = scrollLeft - clientWidth;
+            }
+            if(interaction === 'right') {
+                scrollX = scrollLeft + clientWidth;
+            }
+            console.log(scrollX)
+            contentContainer.scroll({top:0, left:scrollX, behavior:"smooth"})
     }
 
-    if (carouselClick != null) {
-        onSlideClick(carouselClick)
-    }
-})
-
-
-function onSlideClick(carouselClick) {
-    const carousel = carouselClick.closest(".latest-posts").querySelector(".carousel")
-    const carouselIndex = parseInt(getComputedStyle(carousel).getPropertyValue("--carousel-index"))
-    
-    if (carouselClick.classList.contains("left-arrow")) {
-        if(carouselIndex - 1 < 0) {
-            carousel.style.setProperty("--carousel-index", carouselIndex - 0)
-        } else {
-            carousel.style.setProperty("--carousel-index", carouselIndex - 1)
-        }
-    }
-
-    if (carouselClick.classList.contains("right-arrow")) {
-        if(carouselIndex + 1 <= 2) {
-            carousel.style.setProperty("--carousel-index", carouselIndex + 1)
-        }
-        if(carouselIndex + 1 <= 3) {
-            carousel.style.setProperty("--carousel-index", carouselIndex + 1)
-        }
-    }
+    arrowLeft.addEventListener('click', () => handleNavigation('left'));
+    arrowRight.addEventListener('click', () => handleNavigation('right'));
 }
+
+init();
+
+
+
+
+
+
+
+// document.addEventListener("click", carouselEvent => {
+//     let carouselClick
+//     if (carouselEvent.target.matches(".slide-arrow")) {
+//         carouselClick = carouselEvent.target
+//     } else {
+//         carouselClick = carouselEvent.target.closest(".slide-arrow")
+//     }
+
+//     if (carouselClick != null) {
+//         onSlideClick(carouselClick)
+//     }
+// })
+
+
+// function onSlideClick(carouselClick) {
+//     const carousel = carouselClick.closest(".latest-posts").querySelector(".carousel")
+//     const carouselIndex = parseInt(getComputedStyle(carousel).getPropertyValue("--carousel-index"))
+    
+//     if (carouselClick.classList.contains("left-arrow")) {
+//         if(carouselIndex - 1 < 0) {
+//             carousel.style.setProperty("--carousel-index", carouselIndex - 0)
+//         } else {
+//             carousel.style.setProperty("--carousel-index", carouselIndex - 1)
+//         }
+//     }
+
+//     if (carouselClick.classList.contains("right-arrow")) {
+//         // if(carouselIndex + 1 <= 2) {
+//             carousel.style.setProperty("--carousel-index", carouselIndex + 1)
+//         // }
+//         // if(carouselIndex + 1 <= 3) {
+//         //     carousel.style.setProperty("--carousel-index", carouselIndex + 1)
+//         // }
+//     }
+// }
