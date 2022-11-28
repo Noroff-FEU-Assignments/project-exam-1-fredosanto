28583133cad42b174b17ec11589c7190a909f667
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-11-28 16:56:21 +0100

    styling article.html

--- js/header.js
+++ js/header.js
@@ -9,17 +9,6 @@ async function getMainPost() {
         const mainPost = await response.json();
 
         console.log(mainPost[0])
-
-        // headerPost.innerHTML = `<div class="main-post_container main-post_content">
-        //                             <h1>${mainPost[0].name}</h1>
-        //                             <a href="" class="category-tag">Design</a>
-        //                             <p>${mainPost[0].short_description
-        //                             }</p>
-        //                             <a href="#" class="read-tag">Read more</a>
-        //                         </div>
-        //                         <div class="main-post_container main-post_image">
-        //                             <img src="${mainPost[0].images[0].src}" alt="mainPost[0].images[0].alt"/>
-        //                             </div>`;
         
 
         headerPost.innerHTML = `<div class="main-post_container main-post_content">
