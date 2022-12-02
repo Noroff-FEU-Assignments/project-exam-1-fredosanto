dca56c158a73a17964dc70a7a7838f6e46d37994
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-12-02 13:05:14 +0100

    started on lightbox in article.html

--- js/article.js
+++ js/article.js
@@ -4,7 +4,30 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-const url = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts/" + id;
+// const url = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts/" + id;
+const url = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts/" + id + "?_embed";
+
+// async function getPost() {
+//     try {
+//         const response = await fetch(url);
+//         const result = await response.json()
+
+//         console.log(result)
+
+//         document.title = `Post | ${result.title.rendered}`;
+//         contentContainer.innerHTML += `<div class="details_container">
+//                                         <h1 class="details_title">${result.title.rendered}</h1>
+//                                         <div class="details_content">${result.content.rendered}</div>
+//                                         </div>`;
+
+//     }
+
+//     catch(error) {
+//         console.log(error);
+//     }
+// };
+
+// getPost();
 
 async function getPost() {
     try {
@@ -16,6 +39,13 @@ async function getPost() {
         document.title = `Post | ${result.title.rendered}`;
         contentContainer.innerHTML += `<div class="details_container">
                                         <h1 class="details_title">${result.title.rendered}</h1>
+                                        <a class="lightbox-image" href="#lightbox"><img src="${result._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" /></a>
+                                        <div class="lightbox_container" id="lightbox">
+                                            <figure>
+                                                <a href="#" class="lightbox-close"</a>
+                                                <img src="${result._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" />
+                                            </figure>
+                                        </div>
                                         <div class="details_content">${result.content.rendered}</div>
                                         </div>`;
 
