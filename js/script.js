df364a662588315f9440b527e4fabbfe1c7c82f5
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-12-04 13:24:50 +0100

    filter function witch search

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const hamburgerMenu = document.querySelector(".nav-menu");
 
 const searchToggle = document.querySelector(".fa-magnifying-glass");
 const searchField = document.querySelector(".search-field");
+const searchResults = document.querySelector(".results_container");
+const search = document.getElementById("search")
+const urlSearch = "https://fredo.one/nocreaseblog/wp-json/wp/v2/posts?per_page=100&_embed";
+
+
 
 hamburgerToggle.addEventListener('click', function(){
     hamburgerMenu.classList.toggle('active');
@@ -23,3 +28,32 @@ searchToggle.addEventListener('click', function(){
         hamburgerMenu.classList.toggle('active');
     }
 })
+
+async function renderPosts() {
+    try {
+        const response = await fetch(urlSearch);
+        const posts = await response.json()
+
+        console.log(posts);
+
+        posts.forEach((post) => {
+            searchResults.innerHTML += `<div class="result">
+                                            <a href="/article.html?id=${post.id}">
+                                                <img src="${post._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url}" alt="" />
+                                                <h4>${post.title.rendered}</h4>
+                                            </a>
+                                        </div>`;
+        })
+    }
+
+    catch(error) {
+        console.log(error)
+    }
+}
+renderPosts();
+
+search.onclick = function () {
+    console.log(event);
+}
+
+
